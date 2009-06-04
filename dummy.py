fe8c43cf895134ff4e9b68e33b0170f5deaf0899
unknown <Michael@.(none)>
2009-06-04 10:15:10 -0700

    Trying to enable cron

--- dummy.py
+++ dummy.py
@@ -5,34 +5,47 @@ __author__ = 'davidbyttow@google.com (David Byttow)'
 from waveapi import events
 from waveapi import model
 from waveapi import robot
- 
- 
+from google.appengine.ext import db 
+import cgi
+
+class Participant(db.Model):
+  email_to_add = db.StringProperty(multiline=False)
+  added = db.BooleanProperty(default = False)
+  date = db.DateTimeProperty(auto_now_add=True)
+
 def OnParticipantsChanged(properties, context):
   """Invoked when any participants have been added/removed."""
-  added = properties['participantsAdded']
-  for p in added:
-    if p == 'dummy-tutorial@appspot.com':
-      Setup(context)
-      break
 	  
 def OnDocumentChanged(properties, context):
   """Invoked when document changes"""
+  InviteAll(context)
+
+def InviteAll(context):	  
   root_wavelet = context.GetRootWavelet()
-  root_wavelet.CreateBlip().GetDocument().SetText("I spam you!")
- 
- 
-def Setup(context):
+  participants = db.GqlQuery("SELECT * FROM Participant ORDER BY date DESC")
+  for participant in participants:
+    value = participant.email_to_add
+    if value != None and participant.added == False:
+      output = root_wavelet.AddParticipant(cgi.escape(value))
+      participant.added = True
+      participant.put
+	  
+def Announce(context):
   """Called when this robot is first added to the wave."""
   root_wavelet = context.GetRootWavelet()
   root_wavelet.CreateBlip().GetDocument().SetText("I'm alive!")
  
- 
+def OnUpdate(context):
+  root_wavelet = context.GetRootWavelet()
+  root_wavelet.CreateBlip().GetDocument().SetText("I'm a crontask!")
+  
 if __name__ == '__main__':
-  dummy = robot.Robot('Dummy',
-                      image_url='http://dummy.appspot.com/icon.png',
-                      profile_url='http://dummy.appspot.com/')
+  dummy = robot.Robot('Invitation-bot',
+                      image_url='http://invitation-bot.appspot.com/icon.png',
+                      profile_url='http://invitation-bot.appspot.com/')
   dummy.RegisterHandler(events.WAVELET_PARTICIPANTS_CHANGED,
                         OnParticipantsChanged)
   dummy.RegisterHandler(events.DOCUMENT_CHANGED,
-                        OnDocumentChanged)						
+                        OnDocumentChanged)
+  dummy.RegisterCronJob("/_wave/robot/update", 10)						
   dummy.Run()
--- hello.py
+++ hello.py
@@ -7,6 +7,55 @@ from waveapi import events
 from waveapi import model
 from waveapi import robot
 
-print 'Content-Type: text/plain'
-print ''
-print 'Hello, world!'
+import cgi
+
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
+
+class Participant(db.Model):
+  email_to_add = db.StringProperty(multiline=False)
+  added = db.BooleanProperty(default = False)
+  date = db.DateTimeProperty(auto_now_add=True)
+
+class MainPage(webapp.RequestHandler):
+  def get(self):
+    self.response.out.write('<html><body>')
+
+    participants = db.GqlQuery("SELECT * FROM Participant ORDER BY date DESC LIMIT 10")
+
+    for participant in participants:
+      value = participant.email_to_add
+      if value != None:
+        self.response.out.write('<b>%s</b> asked to join:' % cgi.escape(value))
+        self.response.out.write('  Joined field is: <b>%s</b></br>' % participant.added)
+ 
+    # Write the submission form and the footer of the page
+    self.response.out.write("""
+          <form action="/sign" method="post">
+            <div><textarea name="email" rows="3" cols="60"></textarea></div>
+            <div><input type="submit" value="Enter sandbox email"></div>
+          </form>
+          <br/>Users are added any time the wave is updated (I haven't figured out CRON yet) and everyone is re-added (I need to figure out how to set the "added" flag to true).<br/>You can add this robot to a wave to invite everyone who has joined on this web form to that wave.<br/>The source for this wave is available <a href = "http://github.com/MikeSofaer/invitation-bot/tree/master">on my github</a>.
+        </body>
+      </html>""")
+
+class WaveList(webapp.RequestHandler):
+  def post(self):
+    participant = Participant()
+
+    participant.email_to_add = self.request.get('email')
+    participant.put()
+    self.redirect('/')
+
+application = webapp.WSGIApplication(
+                                     [('/', MainPage),
+                                      ('/sign', WaveList)],
+                                     debug=True)
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
